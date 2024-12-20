39defc34d525a9e622a6ae503030b39576868a15
Aadarsh Jha <aadarshjha6783@gmail.com>
2024-12-20 18:08:40 +0000

    Mini logs

--- actions/sample-action.ts
+++ actions/sample-action.ts
@@ -1,4 +1,5 @@
 export const SampleActions = async (input: string) => {
+    console.log('SampleActions', input)
     return {
         type: 'SAMPLE_ACTION',
         payload: input
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,3 +1,4 @@
 export async function GET(req: Request) {
+    console.log("Hello World API REQUEST");
     return Response.json({ message: 'Hello World' });
 }
